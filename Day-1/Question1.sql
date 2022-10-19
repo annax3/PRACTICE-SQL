ff86a6f94f96499b9e1be1c2177bc6ac89c1a35a
Prashant Anand <906120anand@gmail.com>
2022-10-19 22:05:50 +0530

    Update_day1

--- Day-1/Question1.sql
+++ Day-1/Question1.sql
@@ -28,3 +28,89 @@ mysql> desc deposit;
 | ADATE  | date        | YES  |     | NULL    |       |
 +--------+-------------+------+-----+---------+-------+
 5 rows in set (0.03 sec)
+
+ii) Table name-BRANCH
+FIELD NAME DATA TYPE SIZE
+BNAME VARCHAR 20
+CITY VARCHAR 20
+
+
+Ans :
+ 
+mysql> create table BRANCH(BNAME VARCHAR(20), CITY VARCHAR(20));
+Query OK, 0 rows affected (0.18 sec)
+
+mysql> desc branch;
++-------+-------------+------+-----+---------+-------+
+| Field | Type        | Null | Key | Default | Extra |
++-------+-------------+------+-----+---------+-------+
+| BNAME | varchar(20) | YES  |     | NULL    |       |
+| CITY  | varchar(20) | YES  |     | NULL    |       |
++-------+-------------+------+-----+---------+-------+
+2 rows in set (0.01 sec)
+
+mysql> select * from branch;
+Empty set (0.00 sec)
+
+iii) Table name-CUSTOMER
+FIELD NAME DATA TYPE SIZE
+CNAME VARCHAR2 20
+CITY VARCHAR2 20
+
+Ans : 
+mysql> create table CUSTOMER(CNAME VARCHAR(20), CITY VARCHAR(20));
+Query OK, 0 rows affected (0.14 sec)
+
+mysql> desc branch;
++-------+-------------+------+-----+---------+-------+
+| Field | Type        | Null | Key | Default | Extra |
++-------+-------------+------+-----+---------+-------+
+| BNAME | varchar(20) | YES  |     | NULL    |       |
+| CITY  | varchar(20) | YES  |     | NULL    |       |
++-------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+
+mysql> select * from customer;
+Empty set (0.00 sec)
+
+iv) Table name-BORROW
+FIELD NAME DATA TYPE SIZE
+LOANNO VARCHAR 5
+CNAME VARCHAR 20
+BNAME VARCHAR 20
+AMOUNT INT
+
+Ans :
+mysql> create table BORROW(LOANNO VARCHAR(5), CNAME VARCHAR(20),BNAME varchar(20),AMOUNT int););
+Query OK, 0 rows affected (0.17 sec)
+
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ')' at line 1
+mysql> desc BORROW;
++--------+-------------+------+-----+---------+-------+
+| Field  | Type        | Null | Key | Default | Extra |
++--------+-------------+------+-----+---------+-------+
+| LOANNO | varchar(5)  | YES  |     | NULL    |       |
+| CNAME  | varchar(20) | YES  |     | NULL    |       |
+| BNAME  | varchar(20) | YES  |     | NULL    |       |
+| AMOUNT | int         | YES  |     | NULL    |       |
++--------+-------------+------+-----+---------+-------+
+4 rows in set (0.00 sec)
+
+mysql> select * from borrow;
+Empty set (0.00 sec)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
